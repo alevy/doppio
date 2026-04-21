f53ca1fa965e6a751feb92d19453049728eb6f16
Amit Aryeh Levy <amit@amitlevy.com>
2026-04-20 22:02:24 -0700

    add Nix flake and rust-toolchain.toml for dev environment

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = { allowUnfree = true; };
+        };
+      in
+        {
+          devShells.default = pkgs.mkShell {
+            packages = [ pkgs.rustup ];
+          };
+        }
+      );
+}
